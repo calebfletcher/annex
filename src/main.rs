058c1127d604912897734ba0a79e9e5317769a73
Caleb Fletcher <caleb@fletcher.cf>
2022-04-20 18:00:56 +1000

    refactor: split panic handler into module

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
-
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
+mod panic;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
