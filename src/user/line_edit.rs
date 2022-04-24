2a1ed81e1e29a01417f09ada6f33447d5ad2a10d
Caleb Fletcher <caleb@fletcher.cf>
2022-04-24 22:02:15 +1000

    feat: add backspace support for line editing

---
+++ src/user/line_edit.rs
@@ -0,0 +1,75 @@
+use core::{marker, mem};
+
+use alloc::string::String;
+use futures_util::{Stream, StreamExt};
+use pc_keyboard::DecodedKey;
+
+use crate::{print, println, task::keyboard};
+
+pub struct Editor<S>
+where
+    S: Stream<Item = DecodedKey>,
+{
+    input: S,
+    current_line: String,
+}
+
+impl<S: Stream<Item = DecodedKey> + marker::Unpin> Editor<S> {
+    pub fn new(input: S) -> Self {
+        Self {
+            input,
+            current_line: String::new(),
+        }
+    }
+
+    /// Get a line from the user
+    ///
+    /// Does not include the final newline.
+    pub async fn prompt(&mut self, prompt: &str) -> String {
+        print!("{}", prompt);
+        while let Some(key) = self.input.next().await {
+            // Ignore raw keys (Alt, Ctrl, other modifier keys)
+            let key = match key {
+                DecodedKey::RawKey(key) => {
+                    println!("ignoring {:?}", key);
+                    continue;
+                }
+                DecodedKey::Unicode(key) => key,
+            };
+
+            match key {
+                ' '..='~' => {
+                    self.current_line.push(key);
+                }
+                '\n' => {
+                    // Don't include final newline
+                    break;
+                }
+                '\x08' => {
+                    // Backspace
+                    self.current_line.pop();
+                }
+                '\x7F' => {
+                    // Delete
+                    self.current_line.pop();
+                }
+                _ => {
+                    println!("ignoring {:?}", key);
+                }
+            }
+        }
+
+        // Take the current line, and leave a new String instance in its place
+        mem::take(&mut self.current_line)
+    }
+}
+
+pub async fn run() {
+    let stream = keyboard::KeyStream::new();
+    let mut editor = Editor::new(stream);
+
+    loop {
+        let line = editor.prompt("> ").await;
+        println!("got line: {line}");
+    }
+}
