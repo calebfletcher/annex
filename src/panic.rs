028e15c51c7f1d0b2d9133e5fdab6084720c77ad
Caleb Fletcher <caleb@fletcher.cf>
2022-05-12 13:05:16 +1000

    feat(panic): print kernel panics to serial

--- src/panic.rs
+++ src/panic.rs
@@ -1,38 +1,25 @@
+use annex::{serial_print, serial_println};
 use core::panic::PanicInfo;
+use x86_64::instructions::interrupts;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    use crate::screen;
-    use core::fmt::Write;
+    interrupts::disable();
 
-    // If running in a debug build, force the kernel panic to be shown, regardless
-    // of what was happening when it occurred. This can be used to diagnose issues
-    // with the graphics system.
-    #[cfg(debug_assertions)]
-    unsafe {
-        let cnsl = screen::TERMINAL.get_unchecked();
-        cnsl.force_unlock();
-    };
+    serial_println!("kernel panic :(");
+    serial_print!("panicked at ");
 
-    // Try to print an error message if possible
-    if let Ok(cnsl) = screen::TERMINAL.try_get() {
-        if let Some(mut cnsl) = cnsl.try_lock() {
-            writeln!(cnsl, "kernel panic :(").unwrap();
-            write!(cnsl, "panicked at ").unwrap();
-
-            if let Some(&message) = info.message() {
-                let _: Result<_, _> = write!(cnsl, "'{}', ", message);
-            }
-            if let Some(&payload) = info.payload().downcast_ref::<&str>() {
-                let _: Result<_, _> = write!(cnsl, "'{}', ", payload);
-            }
-            if let Some(location) = info.location() {
-                // Ignore error from write macro
-                let _: Result<_, _> = write!(cnsl, "{}", location);
-            }
-            writeln!(cnsl).unwrap();
-        }
+    if let Some(&message) = info.message() {
+        serial_print!("'{}', ", message);
     }
+    if let Some(&payload) = info.payload().downcast_ref::<&str>() {
+        serial_print!("'{}', ", payload);
+    }
+    if let Some(location) = info.location() {
+        // Ignore error from write macro
+        serial_print!("{}", location);
+    }
+    serial_println!();
 
     annex::hlt_loop();
 }
